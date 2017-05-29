16a0927934aac021ccf804ec5c203d5801f0cd1a
fastily <fastily@users.noreply.github.com>
2017-05-28 21:38:32 -0700

    added deb install, move addsmbuser to wiki

--- ubuntu/setup/scripts/mpub.sh
+++ ubuntu/setup/scripts/mpub.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-#: Sets permissions of items in ~/Public to a+r
+#: Sets permissions of items in ~/Public and /mnt/stuff to a+r
 #: 
-#: Tested on Ubuntu Server 15.10.  
+#: Tested on Ubuntu Server 17.04.  
 #: Author: Fastily
 
 printf "Recursively setting contents of ~/Public to a+r\n"
 
 cd ~/Public
+find . -type f -exec chmod 664 {} +
+find . -type d -exec chmod 775 {} +
+
+printf "Recursively setting contents of /mnt/stuff to a+r\n"
+cd /mnt/stuff
 find . -type f -exec chmod 664 {} +
 find . -type d -exec chmod 775 {} +
 
