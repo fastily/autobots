d2f5869ed52828db026c6a0b587163bcebf81c87
fastily <fastily@users.noreply.github.com>
2017-09-12 18:25:37 -0700

    fixed toWebm slowmo and removed 2-pass encode

--- ubuntu/setup/scripts/toWebm.sh
+++ ubuntu/setup/scripts/toWebm.sh
@@ -15,39 +15,36 @@ shopt -s nullglob nocaseglob
 usage()
 {
 	printf "Usage:\n"
-	printf "toWebm.sh [-a] [-t] [-h]\n"
+	printf "toWebm.sh [-a] [-s] [-h]\n"
 	printf "\t-a: Turn audio transcoding on (off by default)\n"
 	printf "\t-s: Enable slow-motion encoding (8x slowdown @ 30fps)\n"
-	printf "\t-t: Specify two pass encoding\n"
 	printf "\t-h: Show this message and exit\n"
 	exit 1
 }
 
 ## Flag which toggles audio
 aSet="-an"
-slowMotion=""
+enableSlowMo=false
 
-## Flag to toggle one-pass encode
-onePass=true
-
-while getopts "ahst" flag; do
+while getopts "ahs" flag; do
 	case "$flag" in
 		a) aSet='-c:a libvorbis -q:a 8' ;;
-		s) slowMotion='-r 30 -vf "setpts=8*PTS"'
-		t) onePass=false ;;
+		s) enableSlowMo=true ;;
 		*) usage ;;
 	esac
 done
 
 cd "$PWD"
 
-if [ "$onePass" = true ]; then
+if [ "$enableSlowMo" = true ]; then
 	for f in *.{mp4,mov,mts}; do
-		ffmpeg -y -i "$f" $slowMotion -c:v libvpx-vp9 -b:v 0 -crf 24 -threads 4 -speed 0 $aSet -f webm "${f%.*}.webm"
+		ffmpeg -y -i "$f" -c:v libvpx-vp9 -b:v 0 -crf 24 -threads 4 -speed 0 -r 30 -vf "setpts=8*PTS" -an -f webm "${f%.*}.webm"
 	done
 else
-	for f in *.{mp4,mov,mts}; do 
-		ffmpeg -i "$f" $slowMotion -c:v libvpx-vp9 -pass 1 -b:v 42000K -threads 4 -speed 4 $aSet -f webm -y /dev/null && 
-		ffmpeg -i "$f" $slowMotion -c:v libvpx-vp9 -pass 2 -b:v 42000K -threads 4 -speed 0 $aSet -f webm "${f%.*}.webm"
+	for f in *.{mp4,mov,mts}; do
+		ffmpeg -y -i "$f" -c:v libvpx-vp9 -b:v 0 -crf 24 -threads 4 -speed 0 $aSet -f webm "${f%.*}.webm"
 	done
 fi
+
+
+
