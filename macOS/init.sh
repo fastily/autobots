9d555b407fee8b73766202b6e8f5247980c6d0ae
fastily <fastily@users.noreply.github.com>
2018-03-28 02:16:38 -0700

    added more defaults write to mac init

--- macOS/init.sh
+++ macOS/init.sh
@@ -57,7 +57,6 @@ brew install youtube-dl
 ## Install Apps
 brew cask install atom
 brew cask install firefox
-#brew cask install inkscape
 brew cask install private-internet-access
 brew cask install postman
 brew cask install sts
@@ -90,19 +89,32 @@ gem install jekyll bundler
 
 ## Misc Settings
 defaults write com.apple.TextEdit RichText -int 0 # Start TextEdit in plain text mode
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf" # Finder searches in the current folder
+
+defaults write com.apple.finder FXDefaultSearchScope -string SCcf # Finder searches in the current folder
+defaults write com.apple.finder ShowRecentTags -bool false # hide recent tags
 
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false # disable autocorrect smart-quotes
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false  # disable autocorrect smart-dashes
 
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true  # don't create .DS_STORE in SMB shares
 
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2HyperlinkAuditingEnabled -bool false # Disable trackers
+# defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2HyperlinkAuditingEnabled -bool false # Disable trackers
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false  # disable auto-opening of downloads
+defaults write com.apple.Safari CanPromptForPushNotifications -bool false # prevent websites from asking for push notifications
+defaults write com.apple.Safari DownloadsClearingPolicy -bool false # don't remove downloads 
+defaults write com.apple.Safari PreloadTopHit -bool false # don't preload top hit in background
+defaults write com.apple.Safari PrintHeadersAndFooters -bool false # don't print webpage headers and footers
+defaults write com.apple.Safari NewTabBehavior -int 1 # new tabs open in empty page
+defaults write com.apple.Safari NewWindowBehavior -int 0 # new windows open to homepage
 defaults write com.apple.Safari IncludeDevelopMenu -bool true # show develop menu
 defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false # match on contains instead of startswith
 
-defaults write com.apple.Terminal SecureKeyboardEntry -bool true #enable terminal secure keyboard entry
+defaults write com.apple.Terminal SecureKeyboardEntry -bool true # enable terminal secure keyboard entry
+defaults write com.apple.Terminal "Default Window Settings" -string Pro # set default terminal window to Pro
+defaults write com.apple.Terminal "Startup Window Settings" -string Pro # set startup window to Pro
+defaults write com.apple.Terminal NewTabWorkingDirectoryBehavior -int 1 # New tabs should open with default working dir
+
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true # prevent Photos from opening when iPhone is connected
 
 ## Setup .ssh
 printf "Creating .ssh folder in home directory\n"
