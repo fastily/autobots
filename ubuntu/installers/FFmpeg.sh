02cab3866de07be747c0250cf9b630e6dcccca42
fastily <fastily@users.noreply.github.com>
2018-07-04 00:32:22 -0700

    remove explicit sudo requirement from ubuntu install FFmpeg

--- ubuntu/installers/FFmpeg.sh
+++ ubuntu/installers/FFmpeg.sh
@@ -7,11 +7,6 @@
 #: Intended for Ubuntu 16.04.4
 #: Author: Fastily
 
-cd "${0%/*}" &> /dev/null
-source ../../shared/autobotUtils.sh
-confirmRunAsRoot
-
-
 ## Setting up folder env variables
 ffmpegNAME="ffmpeg-4.0"
 
@@ -24,9 +19,9 @@ bin="${HOME}/bin"
 mkdir -p "$rootsrc" "$src" "$build" "$bin"
 
 ## Install FFmpeg and dependencies
-apt update
+sudo apt update
 
-apt -y install autoconf automake build-essential cmake libsdl2-dev libtheora-dev \
+sudo apt -y install autoconf automake build-essential cmake libsdl2-dev libtheora-dev \
 libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev \
 pkg-config texinfo zlib1g-dev yasm libx264-dev libmp3lame-dev libopus-dev libfdk-aac-dev \
 flac mercurial libnuma-dev nasm  ||  { printf "installing dependencies failed, cannot proceed.\n"; exit 1; }
@@ -37,7 +32,7 @@ git clone --depth 1 'https://chromium.googlesource.com/webm/libvpx' && \
 cd libvpx && \
 PATH="${bin}:${PATH}" ./configure --prefix="$build" --disable-examples --disable-unit-tests --enable-vp9-highbitdepth --as=yasm && \
 PATH="${bin}:${PATH}" make && \
-make install
+sudo make install
 
 ## Install x265
 cd "$src" && \
@@ -45,7 +40,7 @@ hg clone 'https://bitbucket.org/multicoreware/x265' && \
 cd x265/build/linux && \
 PATH="${bin}:${PATH}" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$build" -DENABLE_SHARED=off ../../source && \
 PATH="${bin}:${PATH}" make && \
-make install
+sudo make install
 
 ## Install FFmpeg
 cd "$src" && \
@@ -72,6 +67,6 @@ PATH="${bin}:${PATH}" PKG_CONFIG_PATH="${build}/lib/pkgconfig" ./configure \
  --enable-nonfree
 
 PATH="${bin}:${PATH}" make && \
-make install
+sudo make install
 
 printf "All done!\n"
