38793a3690483f1b6d1b2c46dc22d40d85711f57
fastily <fastily@users.noreply.github.com>
2018-12-25 13:16:45 -0800

    added basic flag to macos init, split settings from init

--- macOS/init.sh
+++ macOS/init.sh
@@ -4,10 +4,6 @@
 #:
 #: Author: Fastily
 
-## Deploy custom scripts
-cd "${0%/*}" &> /dev/null
-bash scripts/deploy.sh
-
 ## Make sure xcode is installed
 which xcrun &> /dev/null
 if [ $? -ne 0 ]; then
@@ -27,6 +23,16 @@ fi
 
 brew doctor
 brew update
+
+# stop here if --basic flag is set
+if [[ $* == *--basic* ]]; then
+	exit
+fi
+
+## Deploy custom scripts
+cd "${0%/*}" &> /dev/null
+bash scripts/deploy.sh
+bash settings.sh
 
 ## Initialize git name and email
 printf "Setting git global vars\n"
@@ -86,7 +92,7 @@ pip3 install virtualenv
 code --install-extension ms-python.python
 
 mkdir -p ~/'Library/Application Support/Code/User/'
-printf '{"python.linting.pylintArgs": ["--load-plugins=pylint_django"],}' > ~/'Library/Application Support/Code/User/settings.json'
+printf '{"python.linting.pylintArgs": ["--load-plugins=pylint_django"], "python.pythonPath": "/usr/local/bin/python3"}' > ~/'Library/Application Support/Code/User/settings.json'
 
 ## Install global npm packages
 # npm install -g bootlint
@@ -94,43 +100,6 @@ printf '{"python.linting.pylintArgs": ["--load-plugins=pylint_django"],}' > ~/'L
 
 ## Install Gems
 # gem install jekyll bundler
-
-
-## OS Settings
-defaults write com.apple.TextEdit RichText -int 0 # Start TextEdit in plain text mode
-
-defaults write com.apple.finder FXDefaultSearchScope -string SCcf # Finder searches in the current folder
-defaults write com.apple.finder ShowRecentTags -bool false # hide recent tags
-
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false # disable autocorrect smart-quotes
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false  # disable autocorrect smart-dashes
-
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true  # don't create .DS_STORE in SMB shares
-
-## May be broken due to way Mojave sets permissions - wating on workaround
-# defaults write com.apple.Safari AutoOpenSafeDownloads -bool false  # disable auto-opening of downloads
-# defaults write com.apple.Safari CanPromptForPushNotifications -bool false # prevent websites from asking for push notifications
-# defaults write com.apple.Safari DownloadsClearingPolicy -bool false # don't remove downloads 
-# defaults write com.apple.Safari PreloadTopHit -bool false # don't preload top hit in background
-# defaults write com.apple.Safari PrintHeadersAndFooters -bool false # don't print webpage headers and footers
-# defaults write com.apple.Safari NewTabBehavior -int 1 # new tabs open in empty page
-# defaults write com.apple.Safari NewWindowBehavior -int 0 # new windows open to homepage
-# defaults write com.apple.Safari IncludeDevelopMenu -bool true # show develop menu
-# defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false # match on contains instead of startswith
-# defaults write com.apple.Safari ShowOverlayStatusBar -bool true # always show status bar in bottom left corner
-
-defaults -currentHost write com.apple.screensaver idleTime -int 0 # disable screensaver
-
-defaults write com.apple.Terminal SecureKeyboardEntry -bool true # enable terminal secure keyboard entry
-defaults write com.apple.Terminal "Default Window Settings" -string Pro # set default terminal window to Pro
-defaults write com.apple.Terminal "Startup Window Settings" -string Pro # set startup window to Pro
-defaults write com.apple.Terminal NewTabWorkingDirectoryBehavior -int 1 # New tabs should open with default working dir
-
-defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true # prevent Photos from opening when iPhone is connected
-
-## May be broken
-# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad USBMouseStopsTrackpad -bool true # disable trackpad in clamshell mode
-# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true # tap to click
 
 ## Create .ssh and scripts folders
 mkdir -p ~/.ssh ~/Documents/scripts
---
+++ macOS/settings.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+## OS Settings
+defaults write com.apple.TextEdit RichText -int 0 # Start TextEdit in plain text mode
+
+defaults write com.apple.finder FXDefaultSearchScope -string SCcf # Finder searches in the current folder
+defaults write com.apple.finder ShowRecentTags -bool false # hide recent tags
+
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false # disable autocorrect smart-quotes
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false  # disable autocorrect smart-dashes
+
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true  # don't create .DS_STORE in SMB shares
+
+## May be broken due to way Mojave sets permissions - wating on workaround
+# defaults write com.apple.Safari AutoOpenSafeDownloads -bool false  # disable auto-opening of downloads
+# defaults write com.apple.Safari CanPromptForPushNotifications -bool false # prevent websites from asking for push notifications
+# defaults write com.apple.Safari DownloadsClearingPolicy -bool false # don't remove downloads 
+# defaults write com.apple.Safari PreloadTopHit -bool false # don't preload top hit in background
+# defaults write com.apple.Safari PrintHeadersAndFooters -bool false # don't print webpage headers and footers
+# defaults write com.apple.Safari NewTabBehavior -int 1 # new tabs open in empty page
+# defaults write com.apple.Safari NewWindowBehavior -int 0 # new windows open to homepage
+# defaults write com.apple.Safari IncludeDevelopMenu -bool true # show develop menu
+# defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false # match on contains instead of startswith
+# defaults write com.apple.Safari ShowOverlayStatusBar -bool true # always show status bar in bottom left corner
+
+defaults -currentHost write com.apple.screensaver idleTime -int 0 # disable screensaver
+
+defaults write com.apple.Terminal SecureKeyboardEntry -bool true # enable terminal secure keyboard entry
+defaults write com.apple.Terminal "Default Window Settings" -string Pro # set default terminal window to Pro
+defaults write com.apple.Terminal "Startup Window Settings" -string Pro # set startup window to Pro
+defaults write com.apple.Terminal NewTabWorkingDirectoryBehavior -int 1 # New tabs should open with default working dir
+
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true # prevent Photos from opening when iPhone is connected
+
+## May be broken
+# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad USBMouseStopsTrackpad -bool true # disable trackpad in clamshell mode
+# defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true # tap to click
