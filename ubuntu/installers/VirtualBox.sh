78075da358bc8c6b42dd7df4f0f220663125a143
fastily <fastily@users.noreply.github.com>
2019-01-23 18:52:55 -0800

    bump ubuntu vbox install versions

--- ubuntu/installers/VirtualBox.sh
+++ ubuntu/installers/VirtualBox.sh
@@ -6,16 +6,15 @@
 #: Intended for Ubuntu 18.04
 #: Author: Fastily
 
-DLURL='https://download.virtualbox.org/virtualbox/5.2.18/virtualbox-5.2_5.2.18-124319~Ubuntu~bionic_amd64.deb'
-EXTPACKURL='https://download.virtualbox.org/virtualbox/5.2.18/Oracle_VM_VirtualBox_Extension_Pack-5.2.18.vbox-extpack'
-
-FILENAME="$( basename $DLURL )"
+DLURL='https://download.virtualbox.org/virtualbox/6.0.2/virtualbox-6.0_6.0.2-128162~Ubuntu~bionic_amd64.deb'
+EXTPACKURL='https://download.virtualbox.org/virtualbox/6.0.2/Oracle_VM_VirtualBox_Extension_Pack-6.0.2.vbox-extpack'
 TARGETDIR="/tmp/vboxsetup"
+
 mkdir -p "$TARGETDIR"
 
 cd "$TARGETDIR"
 wget "$DLURL"
-sudo dpkg -i "$FILENAME"
+sudo dpkg -i "$( basename $DLURL )"
 sudo apt-get -y -f install
 
 wget -P ~ "$EXTPACKURL"
