f5849fb546a51f92daca8b28ccfce631715840ef
fastily <fastily@users.noreply.github.com>
2019-01-26 20:21:46 -0800

    include pubkey for raspbian install

--- raspbian/util/firstRun.sh
+++ raspbian/util/firstRun.sh
@@ -11,5 +11,6 @@ sudo apt -y install git
 
 cd /tmp
 
-git clone --depth 1 'https://github.com/fastily/autobots.git'
-bash autobots/raspbian/init.sh
+git clone --depth 1 'https://github.com/fastily/autobots.git' && \
+bash autobots/raspbian/init.sh && \
+curl 'https://gist.githubusercontent.com/fastily/4695d2bca65d91958f602bfb8d497def/raw/fd1b441b9f36c620ac0a3f1d9db5cb87b5cd4d3d/pi.pub' >> ~/.ssh/authorized_keys
