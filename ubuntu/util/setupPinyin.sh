34a09e96d854903531c10ca10026bf58344fe945
fastily <fastily@users.noreply.github.com>
2019-03-06 00:26:17 -0800

    added setupPinyin to ubuntu utils

---
+++ ubuntu/util/setupPinyin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#: Installs prerequisites to add pinyin as an input source in ubuntu
+#: 
+#: Tested on Ubuntu 18.04
+#: Author: Fastily
+
+sudo apt update && sudo apt -y install ibus-pinyin
+ibus restart
+
+prtinf "Done!\nNow, go to Settings -> Region & Language -> Input Sources (+) and add Pinyin\n"
