fc7a940e236de3fdd73439dc56fe98fb3fc3cbce
fastily <fastily@users.noreply.github.com>
2019-03-18 02:16:38 -0700

    use better log message for macos ripVideoAudio

--- macOS/scripts/bin/ripVideoAudio.sh
+++ macOS/scripts/bin/ripVideoAudio.sh
@@ -9,10 +9,22 @@
 # Set bash case-insensitivity and set non-matching patterns to expand to nothing
 shopt -s globstar nullglob nocaseglob
 
+##
+# Prints log message to std out
+# PARAMETERS:
+# 		$1 - current file being processed
+##
+ripMessage() {
+	printf '%s\n[INFO]: ripping audio from "%s"...\n' "$(date)" "$1"
+}
+
+
 for f in **/*.{mkv,mts,mt2s,webm,ogv}; do
-	ffmpeg -n -i "$f" -vn -c:a copy -map_metadata 0 -map_metadata:s:a 0:s:a -hide_banner "${f%.*}".mka
+	ripMessage "$f"
+	ffmpeg -n -i "$f" -vn -c:a copy -map_metadata 0 -map_metadata:s:a 0:s:a -loglevel panic "${f%.*}".mka
 done
 
 for f in **/*.{mp4,mov}; do
-	ffmpeg -n -i "$f" -vn -c:a copy -map_metadata 0 -map_metadata:s:a 0:s:a -hide_banner "${f%.*}".m4a
+	ripMessage "$f"
+	ffmpeg -n -i "$f" -vn -c:a copy -map_metadata 0 -map_metadata:s:a 0:s:a -loglevel panic "${f%.*}".m4a
 done
