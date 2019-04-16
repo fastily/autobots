6651be810c792c93b0d7f5bb86c1a1e61abf33dd
fastily <fastily@users.noreply.github.com>
2019-04-16 14:48:02 -0700

    drop m4a requirement from macos ytaudio alias

--- raspbian/init.sh
+++ raspbian/init.sh
@@ -30,4 +30,7 @@ mkdir -p ~/bin
 printf '\nalias uuu="sudo apt update && sudo apt upgrade -y"' >> ~/.bash_aliases
 printf '\nalias aaa="sudo apt autoclean && sudo apt autoremove -y"' >> ~/.bash_aliases
 
+## Configure timezone
+# sudo timedatectl set-timezone UTC
+
 printf "Done!\n"
