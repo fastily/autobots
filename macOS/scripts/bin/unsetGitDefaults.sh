543abde0258a10f825cbe205afb28f342777777a
fastily <fastily@users.noreply.github.com>
2019-10-03 02:14:26 -0700

    new macos script to unset git defaults

---
+++ macOS/scripts/bin/unsetGitDefaults.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+##
+#: If the current directory is a git repo, then unset user.name, user.email, and credential.helper.  Makes it easy to make commits as a different user.
+#: 
+#: Author: Fastily
+##
+
+git config --local user.name "" && \
+git config --local user.email "" && \
+git config --local credential.helper "" && \
+printf "OK\n"
