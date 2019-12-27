ca311b54e202a97732a8bb9ca05a687bf4085364
fastily <fastily@users.noreply.github.com>
2019-12-26 20:54:19 -0800

    minor sanity fixes for raspbian init

--- raspbian/installers/PiHole.sh
+++ raspbian/installers/PiHole.sh
@@ -10,13 +10,13 @@ sudo bash "pi-hole/automated install/basic-install.sh"
 
 
 # setup automatic updates via cron - Saturdays @ 14:00 UTC
-CTF="/tmp/currctab"
+# CTF="/tmp/currctab"
 
-if crontab -l &> /dev/null; then
-	crontab -l > /tmp/currctab
-else
-	printf 'PATH=/home/pi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games\nSHELL=/bin/bash\n\n' >> "$CTF"
-fi
+# if crontab -l &> /dev/null; then
+# 	crontab -l > /tmp/currctab
+# else
+# 	printf 'PATH=/home/pi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games\nSHELL=/bin/bash\n\n' >> "$CTF"
+# fi
 
-printf "0 14 * * 6 { date; pihole -up; echo; } >> ${HOME}/pihole-update-log.txt 2>&1\n" >> "$CTF"
-crontab "$CTF"
+# printf "0 14 * * 6 { date; pihole -up; echo; } >> ${HOME}/pihole-update-log.txt 2>&1\n" >> "$CTF"
+# crontab "$CTF"
