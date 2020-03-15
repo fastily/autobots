b6fd5193133dfd507bfb97d386725090404a93bb
fastily <fastily@users.noreply.github.com>
2020-03-15 15:54:46 -0700

    get macos clone-git-projects up to date

--- macOS/util/clone-git-projects.sh
+++ macOS/util/clone-git-projects.sh
@@ -3,15 +3,14 @@
 #: Clones my git repositories for development.
 #:
 #: PRECONDITION:
-#:		1) my ssh keys have been installed
+#:		1) I am logged into git
 #:
 #: Author: Fastily
 
 GIT_DIR=~/"Documents/git"
-WORKSPACE_DIR=~/"Documents/workspace"
+# WORKSPACE_DIR=~/"Documents/workspace"
 
 GITHUB='https://github.com/fastily'
-BITBUCKET='git@bitbucket.org:fastily'
 
 ##
 # Clones and builds Java projects
@@ -21,35 +20,35 @@ BITBUCKET='git@bitbucket.org:fastily'
 # 		$3 - additional options to pass to gradle.  Optional - leave blank to disable.
 ##
 cloneAndBuild() {
-	cd "$WORKSPACE_DIR"
-
-	git clone "${1}/${2}.git" && \
-	cd "$2" && \
-	gradle build $3
-
-	cd "$WORKSPACE_DIR"
+	git clone "https://github.com/fastily/${1}.git"
+	pushd "$1" && \
+	gradle build $2
+	popd
 }
 
 
-mkdir -p "$GIT_DIR" "$WORKSPACE_DIR"
+mkdir -p "$GIT_DIR"
 
 
 ## Setup general projects
 cd "$GIT_DIR"
 git clone "${GITHUB}/backup-utils.git"
 git clone "${GITHUB}/cheatsheet.git"
-git clone "${GITHUB}/fastilybot-toollabs.git"
+git clone "${GITHUB}/fastilybot-toolforge.git"
+git clone "${GITHUB}/fcpx-presets.git"
 git clone "${GITHUB}/ip-range-calc.git"
-# git clone "${GITHUB}/jwiki-starter"
+git clone "${GITHUB}/jinja2html"
+git clone "${GITHUB}/jwiki-starter"
 git clone "${GITHUB}/simple-commons-uploader"
 git clone "${GITHUB}/text2hash.git"
 
+## Java projects
+cloneAndBuild "jwiki" 'publishToMavenLocal -x test'
+cloneAndBuild "wp-toolbox" 'publishToMavenLocal -x test'
+cloneAndBuild "fastilybot"
+cloneAndBuild "mtc"
+cloneAndBuild "flabs"
 
-## Setup eclipse environment
-cloneAndBuild "$GITHUB" "jwiki" 'publishToMavenLocal -x test'
-cloneAndBuild "$GITHUB" "wp-toolbox" 'publishToMavenLocal -x test'
-
-cloneAndBuild "$GITHUB" "fastilybot"
-cloneAndBuild "$GITHUB" "mtc"
-cloneAndBuild "$GITHUB" "flabs"
-# cloneAndBuild "$GITHUB" "mtc-rest"
+## Other
+git clone "${GITHUB}/bootstrap-course.git"
+git clone "${GITHUB}/stardew-valley-saves.git"
