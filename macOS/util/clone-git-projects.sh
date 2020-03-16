2e0b3562e399bc8fb3d9eacc42ef86a7810ea010
fastily <fastily@users.noreply.github.com>
2020-03-16 15:17:43 -0700

    sanity refactor macos clone-git-projects

--- macOS/util/clone-git-projects.sh
+++ macOS/util/clone-git-projects.sh
@@ -8,8 +8,6 @@
 #: Author: Fastily
 
 GIT_DIR=~/"Documents/git"
-# WORKSPACE_DIR=~/"Documents/workspace"
-
 GITHUB='https://github.com/fastily'
 
 ##
@@ -20,27 +18,20 @@ GITHUB='https://github.com/fastily'
 # 		$3 - additional options to pass to gradle.  Optional - leave blank to disable.
 ##
 cloneAndBuild() {
-	git clone "https://github.com/fastily/${1}.git"
+	git clone "${GITHUB}/${1}.git"
 	pushd "$1" && \
 	gradle build $2
 	popd
 }
 
-
 mkdir -p "$GIT_DIR"
-
+cd "$GIT_DIR"
 
 ## Setup general projects
-cd "$GIT_DIR"
-git clone "${GITHUB}/backup-utils.git"
-git clone "${GITHUB}/cheatsheet.git"
-git clone "${GITHUB}/fastilybot-toolforge.git"
-git clone "${GITHUB}/fcpx-presets.git"
-git clone "${GITHUB}/ip-range-calc.git"
-git clone "${GITHUB}/jinja2html"
-git clone "${GITHUB}/jwiki-starter"
-git clone "${GITHUB}/simple-commons-uploader"
-git clone "${GITHUB}/text2hash.git"
+repos=( "backup-utils" "bootstrap-course" "cheatsheet" "fastilybot-toolforge" "fcpx-presets" "ip-range-calc" "jinja2html" "jwiki-starter" "simple-commons-uploader" "stardew-valley-saves" "text2hash" )
+for repo in "${repos[@]}"; do
+	git clone "${GITHUB}/${repo}.git"
+done
 
 ## Java projects
 cloneAndBuild "jwiki" 'publishToMavenLocal -x test'
@@ -48,7 +39,3 @@ cloneAndBuild "wp-toolbox" 'publishToMavenLocal -x test'
 cloneAndBuild "fastilybot"
 cloneAndBuild "mtc"
 cloneAndBuild "flabs"
-
-## Other
-git clone "${GITHUB}/bootstrap-course.git"
-git clone "${GITHUB}/stardew-valley-saves.git"
