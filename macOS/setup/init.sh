2e8dcad6423b32b551b0a576eb94691a97d68eed
fastily <fastily@users.noreply.github.com>
2020-04-04 15:03:22 -0700

    use brew version of java in macos init

--- macOS/setup/init.sh
+++ macOS/setup/init.sh
@@ -34,8 +34,9 @@ git config --global user.name "fastily"
 git config --global user.email 'fastily@users.noreply.github.com'
 
 ## Install script dependencies
-brew tap AdoptOpenJDK/openjdk
-brew cask install adoptopenjdk11
+# brew tap AdoptOpenJDK/openjdk
+# brew cask install adoptopenjdk11
+brew cask install java
 
 brew install bash-completion exiftool ffmpeg gradle imagemagick nmap nodejs python unrar wget youtube-dl
 #brew install eye-d3
