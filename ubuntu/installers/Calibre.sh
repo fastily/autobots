90328657ef22918338cf6169d99505a573f9c28f
fastily <fastily@users.noreply.github.com>
2020-06-07 17:53:23 -0700

    improve docker ubuntu installer, add ubuntu calibre

---
+++ ubuntu/installers/Calibre.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#: Installs latest version of Calibre
+#: 
+#: Tested on Ubuntu Desktop 18.04
+#: Author: Fastily
+
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
--- ubuntu/installers/Docker.sh
+++ ubuntu/installers/Docker.sh
@@ -10,5 +10,11 @@ sudo apt update && sudo apt -y install apt-transport-https ca-certificates curl 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 sudo apt update && sudo apt -y install docker-ce docker-ce-cli containerd.io
+
+# allow current user to run docker
+sudo usermod -a -G docker $(whoami)
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
