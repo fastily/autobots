379d7437d63b56481df44f4697bbc5413477f133
fastily <fastily@users.noreply.github.com>
2020-08-18 17:01:27 -0700

    typo in ubuntu installers chrome

--- ubuntu/installers/Chrome.sh
+++ ubuntu/installers/Chrome.sh
@@ -10,5 +10,5 @@ DEB_FILE="google-chrome-stable_current_amd64.deb"
 cd /tmp
 
 wget "https://dl.google.com/linux/direct/${DEB_FILE}" && \
-sudo dpkg -i "DEB_FILE" && \
+sudo dpkg -i "$DEB_FILE" && \
 sudo apt-get install -f
