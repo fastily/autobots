9fec4ae8a2f0fca756785c2e5c0b71869b500712
fastily <fastily@users.noreply.github.com>
2022-05-26 14:42:15 -0700

    pia ubuntu install automatically enable background daemon

--- ubuntu/installers/PIA-app.sh
+++ ubuntu/installers/PIA-app.sh
@@ -7,5 +7,7 @@
 PIA_VERSION="3.3.1-06924"
 RUN_FILE="pia-linux-${PIA_VERSION}.run"
 
-cd "/tmp"
-wget 'https://installers.privateinternetaccess.com/download/${RUN_FILE}' && sh "$RUN_FILE"
+cd /tmp
+wget "https://installers.privateinternetaccess.com/download/${RUN_FILE}" && sh "$RUN_FILE"
+
+piactl background enable
