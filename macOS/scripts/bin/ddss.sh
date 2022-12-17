fc6f8bc496825d8a85e6fa8979ea08fa7f5839b3
fastily <fastily@users.noreply.github.com>
2022-12-17 14:12:22 -0800

    sanity refactor macos ddss

--- macOS/scripts/bin/ddss.sh
+++ macOS/scripts/bin/ddss.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
 ##
-#: Recursively deletes .DS_STORE from the specified directory.  Deletes starting in the current directory by default.
+#: Recursively deletes .DS_STORE from the specified directory.  Defaults to searching the current working directory.
 #: 
 #: PARAMETERS:
-#: 		$1 - Directory to start looking in.  Optional parameter, leave blank to disable.
+#: 		$1 - Directory to look in.  Optional, leave blank to disable.
+#:
 #: Author: Fastily
 ##
 
-search="."
-if [ $# -gt 0 ]; then
-    search="$1"
-fi
+[ -z "$1" ] && d="." || d="$1"
 
-printf "Recursively deleting '.DS_STORE' from '%s':\n" "$search"
-find "$search" -name '*.DS_Store' -type f -print -delete
+printf "Recursively deleting '.DS_STORE' from '%s'\n" "$d"
+find "$d" -name '*.DS_Store' -type f -print -delete
 
-printf "Done!\n"
+echo "Done!"
