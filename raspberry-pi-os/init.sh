8e2756d6c3912c40806c467a7a7f8533a0989844
fastily <fastily@users.noreply.github.com>
2023-03-15 02:56:33 -0700

    add docker aliases to rpi init

--- raspberry-pi-os/init.sh
+++ raspberry-pi-os/init.sh
@@ -19,7 +19,7 @@ sudo sed -i -E 's/^\#?PermitRootLogin .*/PermitRootLogin no/' "$sshdConfig"
 sudo sed -i -E 's/^\#?PasswordAuthentication .*/PasswordAuthentication no/' "$sshdConfig"
 
 mkdir -p ~/.ssh
-printf 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDVS+ueZU1A4iHGyEKlQ0RzjcU9cio708BpFMZJeGGaP chungus_pi\n' >> ~/.ssh/authorized_keys
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDVS+ueZU1A4iHGyEKlQ0RzjcU9cio708BpFMZJeGGaP chungus_pi' >> ~/.ssh/authorized_keys
 
 sudo systemctl enable ssh
 sudo systemctl start ssh
@@ -31,10 +31,12 @@ cp -v util/* ~/bin
 
 cat << EOF >> ~/".bash_aliases"
 
+alias dcla="docker container ls -a "
+alias dlf="docker logs -f --tail 1000 "
 alias uuaa="sudo apt update && sudo apt upgrade -y && sudo apt autoclean && sudo apt autoremove -y"
 alias uuaar="uuaa && sleep 5 && sudo reboot"
 alias uuaas="uuaa && sleep 5 && sudo shutdown now"
 alias showRaspbianVersion="cat /etc/os-release"
 EOF
 
-printf "Done!\n"
+echo "Done!"
