bb5c4691d46ead5c3c28e5b9bacf51bef6e3806f
fastily <fastily@users.noreply.github.com>
2023-07-16 22:44:25 -0700

    revise rpi docker installer for aarch64

--- raspberry-pi-os/installers/Docker.sh
+++ raspberry-pi-os/installers/Docker.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-#: Installs Docker on Raspberry Pi OS.  Adapted from [https://docs.docker.com/engine/install/debian/#install-using-the-convenience-script]
+#: Installs Docker on Raspberry Pi OS.  Adapted from [https://docs.docker.com/engine/install/debian/]
+#:
+#: PRECONDITION:
+#:      $1 - Must be using 64-bit Raspberry Pi OS
 #: 
 #: Author: Fastily
 
-SCRIPT_NAME="get-docker.sh"
+set -e
 
-cd /tmp
-curl -fsSL "https://get.docker.com" -o "$SCRIPT_NAME"
-sudo sh "$SCRIPT_NAME"
+DOCKER_KEYRING="/etc/apt/keyrings/docker.gpg"
+
+sudo apt update && sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL "https://download.docker.com/linux/debian/gpg" | sudo gpg --dearmor -o "$DOCKER_KEYRING"
+sudo chmod a+r "$DOCKER_KEYRING"
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=${DOCKER_KEYRING}] https://download.docker.com/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 sudo usermod -aG docker "$USER"
