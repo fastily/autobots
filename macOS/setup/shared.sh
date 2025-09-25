98eb93a928e2985d2047320dec6f696a8130712a
fastily <fastily@users.noreply.github.com>
2025-09-25 00:19:45 -0700

    include deno for macOS personal setup

--- macOS/setup/shared.sh
+++ macOS/setup/shared.sh
@@ -138,7 +138,7 @@ EOF
 # Installs apps/programs for my personal devices
 ##
 setup_personal() {
-    brew install cloudflared exiftool ffmpeg imagemagick iperf3 yt-dlp
+    brew install cloudflared deno exiftool ffmpeg imagemagick iperf3 yt-dlp
     brew install --cask vlc # blackhole-2ch
 
     ## Configure Pixelmator
--- ubuntu/installers/Homebrew.sh
+++ ubuntu/installers/Homebrew.sh
@@ -5,7 +5,6 @@
 #: Intended for Ubuntu 22.04
 #: Author: Fastily
 
-sudo apt update && sudo apt install -y build-essential curl file git procps
-
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo apt update && sudo apt install -y build-essential procps curl file git
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 printf '\neval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"\n' >> "${HOME}/.profile"
