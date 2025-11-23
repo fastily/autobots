815da3e4462fbdb9145ab6da5d3bca3cd9a49e54
fastily <fastily@users.noreply.github.com>
2025-11-23 04:58:36 -0800

    add unmaintained rpi files from main

---
+++ raspberry-pi-os/installers/Samba.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#: Installs Samba
+#: 
+#: Author: Fastily
+
+sudo apt update && sudo apt install -y samba samba-common-bin
+
+sudo sed -i '/\[global\]/a server min protocol = NT1' "/etc/samba/smb.conf" # Enables SMB1, yeah I know this is terrible
