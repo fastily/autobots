befc9838f2e27a14b8ae26dd077cecdd6ac2d590
fastily <fastily@users.noreply.github.com>
2026-06-06 22:56:19 -0700

    ubuntu chrome - only apply pw manager ignore if headed

--- ubuntu/installers/Chrome.sh
+++ ubuntu/installers/Chrome.sh
@@ -13,8 +13,9 @@ wget "https://dl.google.com/linux/direct/${DEB_FILE}" && \
 sudo dpkg -i "$DEB_FILE" && \
 sudo apt-get install -f
 
-# shut up passowrd manager, nobody cares
-CFG_FILE="share/applications/google-chrome.desktop"
-LOCALCONFIG=~/".local/${CFG_FILE}"
-cp "/usr/${CFG_FILE}" "$LOCALCONFIG"
-sed -i -e 's|google-chrome-stable|google-chrome-stable --password-store=basic|' "$LOCALCONFIG"
+if systemctl status display-manager &> /dev/null; then # shut up passowrd manager on headed ubuntu
+    CFG_FILE="share/applications/google-chrome.desktop"
+    LOCALCONFIG=~/".local/${CFG_FILE}"
+    cp "/usr/${CFG_FILE}" "$LOCALCONFIG"
+    sed -i -e 's|google-chrome-stable|google-chrome-stable --password-store=basic|' "$LOCALCONFIG"
+fi
